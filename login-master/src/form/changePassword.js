dae7e9829f83376fd0acf7b033577788fee6be07
techminister <37235596+techminister@users.noreply.github.com>
2020-04-20 15:10:16 +0800

    Update changePassword.js

--- login-master/src/form/changePassword.js
+++ login-master/src/form/changePassword.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styleform.css';
-import sutdLogo from "./imageFile/sutdLogo.png";
+import sutdLogo from "../imageFile/sutdLogo.png";
 import { Link } from "react-router-dom";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
